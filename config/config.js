611271dc7c417909cb3b8fbb65494b17da1252a8
Federico <isma-fb@hotmail.com>
2023-02-18 19:07:56 +0100

    Nodemailer & .env OK

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports =  {
+    USER: process.env.EMAIL_USER,
+    PASSWORD: process.env.EMAIL_PASSWORD
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const { USER, PASSWORD } = require('../config/config')
 const  express = require('express');
-const  router = express.Router();
+const nodemailer = require('nodemailer');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +14,34 @@ router.get('/contact', function(req, res, next) {
 });
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
+});
+
+router.post('/contact',async  (req,res) => {
+  const { email, message } = req.body;
+
+  contentHTML = `
+    <h3>Message received from ${email}</h3>
+    <p>${message}</p>
+  `;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth:{
+      user: USER,
+      pass: PASSWORD
+    }
+  });
+
+  const infoMail = await transporter.sendMail({
+    from: 'Fly Porky - Nodemailer',
+    to: 'federicodaw@gmail.com',
+    subject: 'Nodemailer con nodejs',
+    html: contentHTML
+  });
+
+  console.log('Messege send', infoMail.messageId);
+
+  res.send('received')
 });
 
 module.exports = router;
